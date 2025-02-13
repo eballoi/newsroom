ff7a364264794c9a84bc16219dda39ba191dc9c3
Edoardo Balloi <edoardo.balloi@gmail.com>
2025-02-13 17:26:36 +0100

    feat: more ❤️ to the footer

--- src/_shared/components/Footer/Footer.tsx
+++ src/_shared/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 import { Text } from "@/_shared/components/Text";
 export const Footer: FC = () => {
 	return (
-		<footer className="bg-gray-200 text-center py-4 mt-8">
+		<footer className="bg-gray-100 text-center py-4">
 			<Text variant="p3" className="text-sm">
 				<span>© 2025 Newsroom by Edoardo Balloi. All rights reserved.</span>
 			</Text>
