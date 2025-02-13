ac5a978902e1e64290c3d14b769f86798099c2bd
Edoardo Balloi <edoardo.balloi@gmail.com>
2025-02-13 17:09:03 +0100

    feat: +footer

--- src/post/components/AvatarPlaceholder/AvatarPlaceholder.tsx
+++ src/post/components/AvatarPlaceholder/AvatarPlaceholder.tsx
@@ -1,15 +1,18 @@
 import { FC } from "react";
 import { AvatarPlaceholderProps } from "./types";
 import { getInitials } from "./utils";
+import { Text } from "@/_shared/components/Text";
 
 export const AvatarPlaceholder: FC<AvatarPlaceholderProps> = ({ name }) => {
 	const initials = getInitials(name).toUpperCase();
 
 	return (
 		<div
-			className={`w-8 h-8 bg-gray-400 flex items-center justify-center rounded-full text-white text-m`}
+			className={`w-8 h-8 bg-gray-500 flex items-center justify-center rounded-full text-m`}
 		>
-			{initials}
+			<Text variant="small" className="font-semibold">
+				{initials}
+			</Text>
 		</div>
 	);
 };
